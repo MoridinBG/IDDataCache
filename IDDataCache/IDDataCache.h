e30d6a92854837bf1bbe91693836f007c16fdf30
Ivan Dilchovski <root.darkstar@gmail.com>
2015-05-29 20:03:49 +0200

    Initial commit

---
+++ IDDataCache/IDDataCache.h
@@ -0,0 +1,19 @@
+//
+//  IDDataCache.h
+//  IDDataCache
+//
+//  Created by Ivan Dilchovski on 5/29/15.
+//  Copyright (c) 2015 TechLight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IDDataCache.
+FOUNDATION_EXPORT double IDDataCacheVersionNumber;
+
+//! Project version string for IDDataCache.
+FOUNDATION_EXPORT const unsigned char IDDataCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IDDataCache/PublicHeader.h>
+
+
